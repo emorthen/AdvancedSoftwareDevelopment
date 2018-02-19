9dc49b91028cedf0b3f3c66047f57b234eddda02
Erlend M <erlend.morthen@gmail.com>
2018-02-19 12:22:40 +0100

    cleanup

--- AdvancedSoftwareDevelopment/urls.py
+++ AdvancedSoftwareDevelopment/urls.py
@@ -17,9 +17,9 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 
-
 urlpatterns = [
     url('^', include('django.contrib.auth.urls')),
+    # url(r'^login/$', auth_views.login, name='login'),
     url(r'^webshop/', include('webshop.urls')),
     url(r'^admin/', admin.site.urls),
 ]
