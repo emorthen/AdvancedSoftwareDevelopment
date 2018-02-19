4b5e766feb334c9ddab4cbe94f0bc50d85eecb15
Erlend M <erlend.morthen@gmail.com>
2018-02-19 11:47:28 +0100

    add simple login view alternative

--- AdvancedSoftwareDevelopment/urls.py
+++ AdvancedSoftwareDevelopment/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
     url(r'^webshop/', include('webshop.urls')),
     url(r'^admin/', admin.site.urls),
 ]
