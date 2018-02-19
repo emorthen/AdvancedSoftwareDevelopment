ed33b8f47c99be5f15d2bad15fe8ae5c065d1530
Erlend M <erlend.morthen@gmail.com>
2018-02-19 16:49:51 +0100

    edit in model

--- webshop/models.py
+++ webshop/models.py
@@ -3,7 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Product(models.Model):
-    productID = models.CharField(max_length=10, primary_key=True)
+    # id automatically added by django
+    # productID = models.CharField(max_length=10, primary_key=True)
     name = models.CharField(max_length=100)
     desription = models.CharField(max_length=500)
     price = models.IntegerField()
