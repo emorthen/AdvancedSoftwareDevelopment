f1fb0ffe188a93587d92cd450c8883581fd0f3d8
Erlend M <erlend.morthen@gmail.com>
2018-02-22 10:10:18 +0100

    temporary commit, add /home

--- webshop/views.py
+++ webshop/views.py
@@ -8,6 +8,10 @@ def index(request):
     return render(request, 'index.html')
 
 
+def home(request):
+    return render(request, 'home.html')
+
+
 def signup(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
@@ -17,7 +21,20 @@ def signup(request):
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-            return redirect('home')
+            return redirect('login')
     else:
         form = UserCreationForm()
     return render(request, 'signup.html', {'form': form})
+
+
+def my_view(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return redirect('home')
+        ...
+    else:
+        # Return an 'invalid login' error message.
+        ...
