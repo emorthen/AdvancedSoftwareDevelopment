50dc2838ebe1cca1ae8eaa19358eba0f5303bc73
Anniken Holst <annikekh@stud.ntnu.no>
2018-03-01 09:22:06 +0100

    Bugfix

--- AdvancedSoftwareDevelopment/settings.py
+++ AdvancedSoftwareDevelopment/settings.py
@@ -34,7 +34,6 @@ LOGOUT_REDIRECT_URL = 'login'
 
 INSTALLED_APPS = [
     'cart',
-    'webshop',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- webshop/admin.py
+++ webshop/admin.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
-from .models import Product, ProductAttribute, ProductDetail, Catalog
+from .models import Product
 
 # Register your models here.
 admin.site.register(Product)
-admin.site.register(Catalog)
-admin.site.register(ProductDetail)
-admin.site.register(ProductAttribute)
