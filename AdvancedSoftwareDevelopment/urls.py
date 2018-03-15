a13d964c7f0f6498b9a023cb8a58c6aa76158b28
Erlend M <erlend.morthen@gmail.com>
2018-03-15 10:03:20 +0100

    add spacing

--- AdvancedSoftwareDevelopment/urls.py
+++ AdvancedSoftwareDevelopment/urls.py
@@ -15,6 +15,6 @@ urlpatterns = [
     url(r'^add_to_cart/(\d+)/$', views.add_to_cart, name='add_to_cart'),
     url(r'^remove_from_cart/(\d+)/$', views.remove_from_cart, name='remove_from_cart'),
     url(r'^product_search_list_view/$', views.ProductSearchListView.as_view(), name='product_search_list_view'),
-    url(r'^buy/$',views.remove_all_from_cart,name='Buy'),
+    url(r'^buy/$', views.remove_all_from_cart, name='Buy'),
 
 ]
