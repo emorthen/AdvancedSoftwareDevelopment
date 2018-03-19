04c497ff2fd404abe597e0ce39937d7b5bcc50d2
Anniken Holst <annikekh@stud.ntnu.no>
2018-03-19 09:03:34 +0100

    Removed cart from INSTALLED_APPS in Settings.py

--- AdvancedSoftwareDevelopment/settings.py
+++ AdvancedSoftwareDevelopment/settings.py
@@ -34,7 +34,6 @@ LOGOUT_REDIRECT_URL = 'login'
 # Application definition
 
 INSTALLED_APPS = [
-    'cart',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
