97a284c5458530d923ddf8fc10f4b185a42fd5f8
Tomas Slagstad <tomasl@hotmail.no>
2018-03-19 10:59:17 +0100

    import django_filters

--- AdvancedSoftwareDevelopment/settings.py
+++ AdvancedSoftwareDevelopment/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'webshop.apps.WebshopConfig'
+    'webshop.apps.WebshopConfig',
+    'django_filters'
 ]
 
 MIDDLEWARE = [
