5105b0dc1f8a6e9878fe411c42ce973f824526b5
Erlend M <erlend.morthen@gmail.com>
2018-03-23 18:25:50 +0100

    set debug to false

--- AdvancedSoftwareDevelopment/settings.py
+++ AdvancedSoftwareDevelopment/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = file.readline()
 file.close()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.99.101', '192.168.99.100', '0.0.0.0']
 
